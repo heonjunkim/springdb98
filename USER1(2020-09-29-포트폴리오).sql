546a7cdb3713d5bbb267743800dbd49cca736428
heonjunkim <maplegjswns@naver.com>
2020-10-06 09:03:55 +0900

    table

--- USER1(2020-09-29-포트폴리오).sql
+++ USER1(2020-09-29-포트폴리오).sql
@@ -1,14 +1,49 @@
 
+DROP TABLE tbl_iolist;
+
 CREATE TABLE tbl_iolist (
+
     seq	NUMBER		PRIMARY KEY,
+
     io_date	VARCHAR2(10),		
+
     io_time	VARCHAR2(10),		
-    io_pname	nVARCHAR2(30),		
-    io_input	CHAR(1),		
+
+    io_pname	nVARCHAR2(30) NOT NULL,		
+
+    io_input	CHAR(1) CONSTRAINT input_veri CHECK(io_input ='1' OR io_input = '2'),			
+
     io_price	NUMBER,		
+
     io_quan	NUMBER,		
+
     io_total	NUMBER		
+
 );
+
+CREATE SEQUENCE seq_iolist
+
+START WITH 1 INCREMENT BY 1;
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+
+
+ 
+
+commit;
 
 
 
